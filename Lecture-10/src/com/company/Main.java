70c34331707b988cba1c17363225a0197fbda18b
Julia Zharova <julia.amp.zharova@gmail.com>
2021-12-23 22:43:48 +0200

    Fixed comment

--- Lecture-10/src/com/company/Main.java
+++ Lecture-10/src/com/company/Main.java
@@ -3,14 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    Calc<Integer> calcInt = new Calc<>(1, 2, 3);
-        Double intSum = calcInt.sum();
-        System.out.println(intSum);
-
-        Calc<Double> calcDouble = new Calc<>(1.2, 2.4, 3.7);
-        Double doubleSum = calcDouble.sum();
-        System.out.println(doubleSum);
-
+        Calc<Double> calcDouble = new Calc<>();
         Double intDoubleFloatSum = calcDouble.sum(12, 11.34, 12.1f);
         System.out.println(intDoubleFloatSum);
     }
